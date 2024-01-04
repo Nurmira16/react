b8d3dc4e4e40a5dd4fdef6c0ebe0432e76fc49e4
Nurmira16 <nurmirajorobek@gmail.com>
2024-01-04 17:29:42 +0100

    feat: Create React Portal ModalWrapper component
    This commit introduces a new React component, ModalWrapper, which utilizes React Portals to render modal content outside the normal DOM hierarchy. This component can be used to display modal windows with improved overlay capabilities.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Form from "./Form";
+import ModalWrapper from "./ModalWrapper";
 
 const Modal = ({ closeModal, addData, currentToDo, handleEdit }) => {
   return (
-    <div className="modal">
-      <h1 onClick={closeModal}>Close Modal</h1>
-      <Form
-        handleEdit={handleEdit}
-        currentToDo={currentToDo}
-        addData={addData}
-        closeModal={closeModal}
-      />
-    </div>
+    <ModalWrapper>
+      <div className="modal">
+        <h1 onClick={closeModal}>Close Modal</h1>
+        <Form
+          handleEdit={handleEdit}
+          currentToDo={currentToDo}
+          addData={addData}
+          closeModal={closeModal}
+        />
+      </div>
+    </ModalWrapper>
   );
 };
 
