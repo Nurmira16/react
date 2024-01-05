bf6e68531f447e1b013d13683badcaac176ef224
Nurmira16 <nurmirajorobek@gmail.com>
2024-01-05 11:07:49 +0100

    Add escape event listener for modal window close

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,6 +3,17 @@ import Form from "./Form";
 import ModalWrapper from "./ModalWrapper";
 
 const Modal = ({ closeModal, addData, currentToDo, handleEdit }) => {
+  const keydown = (e) => {
+    if (e.key === "Escape") {
+      closeModal();
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", keydown);
+    return () => {
+      window.removeEventListener("keydown", keydown);
+    };
+  });
   return (
     <ModalWrapper>
       <div className="modalWrapper" onClick={closeModal}>
